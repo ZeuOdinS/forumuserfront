bb1c8ec0132e69086a83763da53bf8ae4bb8fe52
nacim Ghouma <nacimghouma96@gmail.com>
2019-11-13 06:08:04 +0100

    final

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -30,26 +30,7 @@ export class IndexComponent {
     console.log(history.state.langcd);
     this.lang=history.state.langcd;
 
-   /* noUiSlider.create(slider, {
-      start: 40,
-      connect: false,
-      range: {
-        min: 0,
-        max: 100
-      }
-    });
-
-    var slider2 = document.getElementById("sliderDouble");
-
-    noUiSlider.create(slider2, {
-      start: [20, 60],
-      connect: true,
-      range: {
-        min: 0,
-        max: 100
-      }
-    });
-  }*/}
+ }
   ngOnDestroy() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.remove("index-page");
